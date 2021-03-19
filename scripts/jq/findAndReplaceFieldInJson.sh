5668b491f77802fa95b239c0c70a2bea2a61db57
Andre Carvalho <andrecarvalho@MacBook-Pro-de-Andre.local>
2021-03-19 15:24:38 +0000

    Add jq findAndReplaceFieldInJson.sh

--- scripts/jq/findAndReplaceFieldInJson.sh
+++ scripts/jq/findAndReplaceFieldInJson.sh
@@ -1,7 +1,7 @@
 # find all Json files inside a folder and replace the json_field value by a newValue.
 # Note: jq needs "--arg VAR" to pass a variable ('VAR' can be the name you want.)
 
-newValue = "helloWord"
+newValue = "helloWorld"
 
 find destinationFolder/ -name "*.json" | while read line; do 
 	jq --arg VAR "$newValue" '."json_field"=$VAR' $line > temp && mv -f temp $line 
